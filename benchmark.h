600a29eb08691e1645c9c83dc0f0ffc3981a3dc9
leobeltra <leonardobeltrame00@gmail.com>
2025-04-10 14:25:05 +0200

    Added cms-soa hardRW benchmark

--- benchmark.h
+++ benchmark.h
@@ -2,6 +2,8 @@
 #define BENCHMARK_H
 
 #include <benchmark/benchmark.h>
+#include <Eigen/Dense>
+#include <Eigen/Core>
 
 #ifdef SOA_BOOST
     #define MEMBER_ACCESS(NAME) NAME()
@@ -9,17 +11,161 @@
     #define MEMBER_ACCESS(NAME) NAME
 #endif
 
+using Vector3D = Eigen::Vector3d;
+using Matrix3D = Eigen::Matrix3d;
+
+constexpr auto BM_CPUEasyRW_nelem = 10;
+
 // 2 data members, integers, 64 alignment, 10 elements
 template <typename T>
-void BM_CPUMemoryIntensive(benchmark::State &state, T &t) {
-    constexpr auto n_elements = 10;
+void BM_CPUEasyRW(benchmark::State &state, T &t) {
     constexpr auto repetitions = 1000000;
 
     for (auto _ : state) {
         for (size_t _; _ < repetitions; ++_) {
-            for (int i = 0; i < n_elements; ++i) {
+            for (int i = 0; i < BM_CPUEasyRW_nelem; ++i) {
                 t[i].MEMBER_ACCESS(x0) += 2;
                 t[i].MEMBER_ACCESS(x1) += 2;
+            } 
+        }
+    }
+}
+
+constexpr auto BM_CPUEasyCompute_nelem = 10;
+
+// 2 data members, integers, 64 alignment, 10 elements
+template <typename T>
+void BM_CPUEasyCompute(benchmark::State &state, T &t) {
+    constexpr auto repetitions = 1000000;
+
+    for (auto _ : state) {
+        for (size_t _; _ < repetitions; ++_) {
+            for (int i = 0; i < BM_CPUEasyCompute_nelem; ++i) {
+                t[i].MEMBER_ACCESS(x0) = 1 + t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x0) * 
+                                         t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x0) + 
+                                         7 * t[i].MEMBER_ACCESS(x1) * t[i].MEMBER_ACCESS(x1) * 
+                                         t[i].MEMBER_ACCESS(x1) - 
+                                         6 * t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x1) * 
+                                         t[i].MEMBER_ACCESS(x1) + 
+                                         3 * t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x0) * 
+                                         t[i].MEMBER_ACCESS(x1);
+                t[i].MEMBER_ACCESS(x1) = 1 + t[i].MEMBER_ACCESS(x1) * t[i].MEMBER_ACCESS(x1) * t[i].MEMBER_ACCESS(x1) * 
+                                         t[i].MEMBER_ACCESS(x1) - 
+                                         5 * t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x0) * 
+                                         t[i].MEMBER_ACCESS(x0) + 
+                                         4 * t[i].MEMBER_ACCESS(x0) * t[i].MEMBER_ACCESS(x1) * 
+                                         t[i].MEMBER_ACCESS(x1) - 
+                                         2 * t[i].MEMBER_ACCESS(x1) * t[i].MEMBER_ACCESS(x0) * 
+                                         t[i].MEMBER_ACCESS(x0);
+            } 
+        }
+    }
+}
+
+constexpr auto BM_CPUHardRW_nelem = 10e4;
+
+// 100 data members (20 floats, 20 doubles, 20 integers, 20 Eigen vector, 20 Eigen matrices), 
+// 64 alignment, 10e4 elements
+template <typename T>
+void BM_CPUHardRW(benchmark::State &state, T &t) {
+    constexpr auto repetitions = 1;
+
+    Matrix3D m; 
+    m << 2, 2, 2, 2, 2, 2, 2, 2, 2;
+    Vector3D v(2, 2, 2);
+
+    for (auto _ : state) {
+        for (size_t _; _ < repetitions; ++_) {
+            for (int i = 0; i < BM_CPUHardRW_nelem; ++i) {
+                t[i].MEMBER_ACCESS(x0)  += 2.f;
+                t[i].MEMBER_ACCESS(x1)  += 2.f;
+                t[i].MEMBER_ACCESS(x2)  += 2.f;
+                t[i].MEMBER_ACCESS(x3)  += 2.f;
+                t[i].MEMBER_ACCESS(x4)  += 2.f;
+                t[i].MEMBER_ACCESS(x5)  += 2.f;
+                t[i].MEMBER_ACCESS(x6)  += 2.f;
+                t[i].MEMBER_ACCESS(x7)  += 2.f;
+                t[i].MEMBER_ACCESS(x8)  += 2.f;
+                t[i].MEMBER_ACCESS(x9)  += 2.f;
+                t[i].MEMBER_ACCESS(x10) += 2.f;
+                t[i].MEMBER_ACCESS(x11) += 2.f;
+                t[i].MEMBER_ACCESS(x12) += 2.f;
+                t[i].MEMBER_ACCESS(x13) += 2.;
+                t[i].MEMBER_ACCESS(x14) += 2.;
+                t[i].MEMBER_ACCESS(x15) += 2.;
+                t[i].MEMBER_ACCESS(x16) += 2.;
+                t[i].MEMBER_ACCESS(x17) += 2.;
+                t[i].MEMBER_ACCESS(x18) += 2.;
+                t[i].MEMBER_ACCESS(x19) += 2.;
+                t[i].MEMBER_ACCESS(x20) += 2.;
+                t[i].MEMBER_ACCESS(x21) += 2.;
+                t[i].MEMBER_ACCESS(x22) += 2.;
+                t[i].MEMBER_ACCESS(x23) += 2.;
+                t[i].MEMBER_ACCESS(x24) += 2.;
+                t[i].MEMBER_ACCESS(x25) += 2.;
+                t[i].MEMBER_ACCESS(x26) += 2;
+                t[i].MEMBER_ACCESS(x27) += 2;
+                t[i].MEMBER_ACCESS(x28) += 2;
+                t[i].MEMBER_ACCESS(x29) += 2;
+                t[i].MEMBER_ACCESS(x30) += 2;
+                t[i].MEMBER_ACCESS(x31) += 2;
+                t[i].MEMBER_ACCESS(x32) += 2;
+                t[i].MEMBER_ACCESS(x33) += 2;
+                t[i].MEMBER_ACCESS(x34) += 2;
+                t[i].MEMBER_ACCESS(x35) += 2;
+                t[i].MEMBER_ACCESS(x36) += 2;
+                t[i].MEMBER_ACCESS(x37) += 2;
+                t[i].MEMBER_ACCESS(x38) += 2;
+                t[i].MEMBER_ACCESS(x39) += v;
+                t[i].MEMBER_ACCESS(x40) += v;
+                t[i].MEMBER_ACCESS(x41) += v;
+                t[i].MEMBER_ACCESS(x42) += v;
+                t[i].MEMBER_ACCESS(x43) += v;
+                t[i].MEMBER_ACCESS(x44) += v;
+                t[i].MEMBER_ACCESS(x45) += v;
+                t[i].MEMBER_ACCESS(x46) += v;
+                t[i].MEMBER_ACCESS(x47) += v;
+                t[i].MEMBER_ACCESS(x48) += v;
+                t[i].MEMBER_ACCESS(x49) += v;
+                t[i].MEMBER_ACCESS(x50) += v;
+                t[i].MEMBER_ACCESS(x51) += m;
+                t[i].MEMBER_ACCESS(x52) += m;
+                t[i].MEMBER_ACCESS(x53) += m;
+                t[i].MEMBER_ACCESS(x54) += m;
+                t[i].MEMBER_ACCESS(x55) += m;
+                t[i].MEMBER_ACCESS(x56) += m;
+                t[i].MEMBER_ACCESS(x57) += m;
+                t[i].MEMBER_ACCESS(x58) += m;
+                t[i].MEMBER_ACCESS(x59) += m;
+                t[i].MEMBER_ACCESS(x60) += m;
+                t[i].MEMBER_ACCESS(x61) += m;
+                t[i].MEMBER_ACCESS(x62) += m;
+                t[i].MEMBER_ACCESS(x63) += m;
+            } 
+        }
+    }
+}
+
+// “Realistic case”: 
+//      10 data members (3 doubles, 3 float, 2 integer, 1 Vector3D, 1 Matrix), 64 alignment, 100000
+template <typename T>
+void BM_CPURealRW(benchmark::State &state, T &t) {
+    constexpr auto n_elements = 100000;
+    constexpr auto repetitions = 1;
+
+    for (auto _ : state) {
+        for (size_t _; _ < repetitions; ++_) {
+            for (int i = 0; i < n_elements; ++i) {
+                t[i].MEMBER_ACCESS(x0) += (double) 2;
+                t[i].MEMBER_ACCESS(x1) += (double) 2;
+                t[i].MEMBER_ACCESS(x2) += (double) 2;
+                t[i].MEMBER_ACCESS(x3) += (float) 2;
+                t[i].MEMBER_ACCESS(x4) += (float) 2;
+                t[i].MEMBER_ACCESS(x5) += (float)2;
+                t[i].MEMBER_ACCESS(x6) += 2;
+                t[i].MEMBER_ACCESS(x7) += 2;
+                t[i].MEMBER_ACCESS(x8) += Vector3D{2, 2, 2};
+                t[i].MEMBER_ACCESS(x9) += Matrix3D{2, 2, 2, 2, 2, 2, 2, 2, 2};
             } 
         }
     }
--- soa_boost.cpp
+++ soa_boost.cpp
@@ -2,6 +2,8 @@
 
 #include "benchmark.h"
 #include "SoALayout.h"
+#include <Eigen/Dense>
+#include <Eigen/Core>
 
 GENERATE_SOA_LAYOUT(SoALayout,
     SOA_COLUMN(int, x0),
@@ -9,7 +11,77 @@ GENERATE_SOA_LAYOUT(SoALayout,
 
 using SoA = SoALayout<>;
 using SoAView = SoA::View;
-using SoAConstView = SoA::ConstView;
+using SoAConstView = SoA::ConstView;    
+
+GENERATE_SOA_LAYOUT(BigSoALayout,
+    SOA_COLUMN(float, x0),
+    SOA_COLUMN(float, x1),
+    SOA_COLUMN(float, x2),
+    SOA_COLUMN(float, x3),
+    SOA_COLUMN(float, x4),
+    SOA_COLUMN(float, x5),
+    SOA_COLUMN(float, x6),
+    SOA_COLUMN(float, x7),
+    SOA_COLUMN(float, x8),
+    SOA_COLUMN(float, x9),
+    SOA_COLUMN(float, x10),
+    SOA_COLUMN(float, x11),
+    SOA_COLUMN(float, x12),
+    SOA_COLUMN(double, x13),
+    SOA_COLUMN(double, x14),
+    SOA_COLUMN(double, x15),
+    SOA_COLUMN(double, x16),
+    SOA_COLUMN(double, x17),
+    SOA_COLUMN(double, x18),
+    SOA_COLUMN(double, x19),
+    SOA_COLUMN(double, x20),
+    SOA_COLUMN(double, x21),
+    SOA_COLUMN(double, x22),
+    SOA_COLUMN(double, x23),
+    SOA_COLUMN(double, x24),
+    SOA_COLUMN(double, x25),
+    SOA_COLUMN(int, x26),
+    SOA_COLUMN(int, x27),
+    SOA_COLUMN(int, x28),
+    SOA_COLUMN(int, x29),  
+    SOA_COLUMN(int, x30),
+    SOA_COLUMN(int, x31),
+    SOA_COLUMN(int, x32),
+    SOA_COLUMN(int, x33),
+    SOA_COLUMN(int, x34),
+    SOA_COLUMN(int, x35),
+    SOA_COLUMN(int, x36),
+    SOA_COLUMN(int, x37),
+    SOA_COLUMN(int, x38),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x39),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x40),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x41),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x42),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x43),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x44),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x45),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x46),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x47),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x48),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x49),
+    SOA_EIGEN_COLUMN(Eigen::Vector3d, x50),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x51),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x52),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x53),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x54),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x55),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x56),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x57),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x58),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x59),  
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x60),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x61),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x62),
+    SOA_EIGEN_COLUMN(Eigen::Matrix3d, x63))
+
+using BigSoA = BigSoALayout<>;
+using BigSoAView = BigSoA::View;
+using BigSoAConstView = BigSoA::ConstView;    
 
 std::unique_ptr<std::byte, decltype(std::free) *> buffer{
     reinterpret_cast<std::byte *>(aligned_alloc(SoA::alignment, SoA::computeDataSize(10))), std::free};
@@ -17,6 +89,14 @@ SoA soa(buffer.get(), 10);
 SoAView soaView{soa};
 SoAConstView soaConstView{soa};
 
-BENCHMARK_CAPTURE(BM_CPUMemoryIntensive, cms_soa, soaView);
+std::unique_ptr<std::byte, decltype(std::free) *> bigbuffer{
+    reinterpret_cast<std::byte *>(aligned_alloc(BigSoA::alignment, BigSoA::computeDataSize(BM_CPUHardRW_nelem))), std::free};
+BigSoA bigSoa(bigbuffer.get(), BM_CPUHardRW_nelem);
+BigSoAView bigSoaView{bigSoa};
+BigSoAConstView bigSoaConstView{bigSoa};
+
+BENCHMARK_CAPTURE(BM_CPUEasyRW, cms_soa, soaView);
+
+BENCHMARK_CAPTURE(BM_CPUHardRW, cms_big_soa, bigSoaView);
 
 BENCHMARK_MAIN();
