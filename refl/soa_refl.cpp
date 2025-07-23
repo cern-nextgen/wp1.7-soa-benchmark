8930d81b7c7a7f9ebfc0be268feb3f593bd04c1b
Jolly Chen <jolly.chen@student.uva.nl>
2025-07-23 16:29:44 +0200

    Add benchmarks with reflmempp SoA

---
+++ refl/soa_refl.cpp
@@ -0,0 +1,58 @@
+
+#define __cpp_lib_reflection 20250130 // eccp version
+#define EIGEN_SAFE_TO_USE_STANDARD_ASSERT_MACRO 0
+
+#include "benchmark.h"
+#include <Eigen/Core>
+#include "rmpp.h"
+#include <vector>
+
+
+struct S2 {
+    int &x0, &x1;
+};
+
+struct S10 {
+    float &x0, &x1;
+    double &x2, &x3;
+    int &x4, &x5;
+    Eigen::Vector3d &x6, &x7;
+    Eigen::Matrix3d &x8, &x9;
+};
+
+struct S64 {
+    double &x0, &x1, &x2, &x3, &x4, &x5, &x6, &x7, &x8, &x9, &x10, &x11, &x12;
+    float &x13, &x14, &x15, &x16, &x17, &x18, &x19, &x20, &x21, &x22, &x23, &x24, &x25;
+    int &x26, &x27, &x28, &x29, &x30, &x31, &x32, &x33, &x34, &x35, &x36, &x37, &x38;
+    Eigen::Vector3d &x39, &x40, &x41, &x42, &x43, &x44, &x45, &x46, &x47, &x48, &x49, &x50;
+    Eigen::Matrix3d &x51, &x52, &x53, &x54, &x55, &x56, &x57, &x58, &x59, &x60, &x61, &x62, &x63;
+};
+
+int main(int argc, char** argv) {
+    // Seperate loops to sort the output by benchmark.
+    for (auto n : {10000}) {
+        using SoA = rmpp::AoS2SoA<S2, 64>;
+        auto buffer = reinterpret_cast<std::byte *>(aligned_alloc(64, SoA::ComputeSize(n)));
+        SoA t2a(buffer, n, SoA::ComputeSize(n));
+        benchmark::RegisterBenchmark("BM_CPUEasyRW", BM_CPUEasyRW<SoA>, t2a)->Arg(n)->Unit(benchmark::kMillisecond);
+    }
+
+    // for (auto n : N) {
+    //     auto t2b = factory::default_wrapper<my_vector, S2, wrapper::layout::soa>(n);
+    //     benchmark::RegisterBenchmark("BM_CPUEasyCompute", BM_CPUEasyCompute<decltype(t2b)>, t2b)->Arg(n)->Unit(benchmark::kMillisecond);
+    // }
+
+    // for (auto n : N) {
+    //     auto t10 = factory::default_wrapper<my_vector, S10, wrapper::layout::soa>(n);
+    //     benchmark::RegisterBenchmark("BM_CPURealRW", BM_CPURealRW<decltype(t10)>, t10)->Arg(n)->Unit(benchmark::kMillisecond);
+    // }
+
+    // for (auto n : N) {
+    //     auto t64 = factory::default_wrapper<my_vector, S64, wrapper::layout::soa>(n);
+    //     benchmark::RegisterBenchmark("BM_CPUHardRW", BM_CPUHardRW<decltype(t64)>, t64)->Arg(n)->Unit(benchmark::kMillisecond);
+    // }
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
+}
