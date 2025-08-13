e47985f8f84586d5a8ab1096afc06dc35ba46e49
Oliver Rietmann <oliver.rietmann@cern.ch>
2025-08-13 17:24:57 +0200

    Fix plot

--- plot.py
+++ plot.py
@@ -36,13 +36,14 @@ def plot_results(df, title, out_dir, min_y=-0.000001, max_y=1000):
     # Set the title and labels
     ax.set_title(title)
     ax.set_xlabel('Number of Elements')
-    ax.set_xscale('symlog')
+    ax.set_xscale('log', base=2)
     ax.set_xticks(df_mean['n_elem'].unique(), labels=["{:g}".format(x) for x in df_mean['n_elem'].unique()], minor=False)
 
     ax.set_ylabel(f'Real Time ({df["time_unit"].iloc[0]})')
-    ax.set_yscale('log')
+    #ax.set_yscale('log', base=2)
     ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:g}'.format(y)))
     plt.ylim(min_y, max_y)
+    plt.grid()
 
     # Add a legend
     plt.legend()
@@ -56,18 +57,18 @@ if __name__ == "__main__":
     dirname = sys.argv[1]
 
     results = {}
-    for f, t in zip(['soa_wrapper', 'soa_manual'],
-                    ['Template Metaprogramming SoA', 'Manual SoA']):
+    for f, t in zip(['soa_wrapper_gpu'],
+                    ['AoS vs SoA for the CUDA kernel x+y+z']):
         filename = f"{dirname}/{f}"
 
         # Comment this to plot the results from locally saved json files, without running the benchmarks.
-        subprocess.run([f"{filename}", "--benchmark_out_format=json", f"--benchmark_out={filename}.json",
+        subprocess.run([f"{filename}", "--benchmark_filter=SYNC_GPUAdd_*", "--benchmark_out_format=json", f"--benchmark_out={filename}.json",
                         "--benchmark_counters_tabular=true", "--benchmark_repetitions=3", "--benchmark_min_warmup_time=2"])
 
         results[f] = (read_data(f"{filename}.json"), t)
 
     # Round the y-axis up/down to the nearest power of 10
-    max_y = 10 ** math.ceil(math.log10(max([df[df["aggregate_name"] == "mean"]["real_time"].max() for (df, _) in results.values()])))
-    min_y = 10 ** math.floor(math.log10(min([df[df["aggregate_name"] == "mean"]["real_time"].min() for (df, _) in results.values()])))
+    max_y = max([df[df["aggregate_name"] == "mean"]["real_time"].max() for (df, _) in results.values()])
+    min_y = min([df[df["aggregate_name"] == "mean"]["real_time"].min() for (df, _) in results.values()])
     for (results, t) in results.values():
         plot_results(results, t, dirname, min_y, max_y)
