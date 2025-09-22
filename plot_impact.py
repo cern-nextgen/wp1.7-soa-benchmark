7be830842df18474c981da0a29b5b4329ebbcbfb
Jolly Chen <jolly.chen@student.uva.nl>
2025-09-22 13:31:47 +0200

    Automatic scaling of clim

--- plot_impact.py
+++ plot_impact.py
@@ -7,106 +7,107 @@ values = {
     "runtime_mean": {
         "label": "Mean Runtime",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 710, "stcl": 250, "nbody": 580},
+        "ymax": True
     },
     "runtime_stddev": {
         "label": "Standard Deviation of Runtime over 5 runs",
         "colormap": "RdYlGn_r",
+        "ymax": False
     },
     "instructions": {
         "label": "Number of Retired Instructions",
         "colormap": "Blues",
-        "ymax": {"im": 2.6e10, "stcl": 8e9, "nbody": 1.78e10},
+        "ymax": True
     },
     "fp_arith_inst_retired.scalar": {
         "label": "Number of Scalar Instructions",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 3.7e9, "stcl": 1.67e9, "nbody": 1.38e10},
+        "ymax": True
     },
     "fp_arith_inst_retired.vector": {
         "label": "Number of Vector Instructions",
         "colormap": "RdYlGn",
-        "ymax": {"im": 3.4e9, "stcl": 1.25e9, "nbody": 0.9e5},
+        "ymax": True
     },
     "L1-icache-load-misses": {
         "label": "L1 Instruction Cache Load Misses",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 6.8e7, "stcl": 3.8e7, "nbody": 1.3e6},
+        "ymax": True
     },
     "L1-dcache-loads": {
         "label": "L1 Data Cache Loads",
         "colormap": "Blues",
-        "ymax": {"im": 6.4e9, "stcl": 2.1e9, "nbody": 4.8e9},
+        "ymax": True
     },
     "L1-dcache-load-misses": {
         "label": "L1 Data Cache Load Misses",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 3.7e8, "stcl": 4.9e8, "nbody": 6.4e8},
+        "ymax": True
     },
     "cache-references": {
         "label": "L2 Cache References",
         "colormap": "Blues",
-        "ymax": {"im": 6.1e8, "stcl": 8.2e8, "nbody": 1.6e9},
+        "ymax": True
     },
     "cache-misses": {
         "label": "L2 Cache Misses",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 6.2e8, "stcl": 7.2e8, "nbody": 1.3e6},
+        "ymax": True
     },
     "mem_inst_retired.all_loads": {
         "label": "Memory Loads Retired",
         "colormap": "Blues",
-        "ymax": {"im": 6.3e9, "stcl": 2.1e9, "nbody": 4.8e9},
+        "ymax": True
     },
     "alignment-faults": {
         "label": "Alignment Faults",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 0.1, "stcl": 0.1, "nbody": 0.1},
+        "ymax": True,
     },
     "branch-misses": {
         "label": "Branch Misses",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 2.8e7, "stcl": 3.3e6, "nbody": 3.4e5},
+        "ymax": True,
     },
     "branch-instructions": {
         "label": "Branch Instructions",
         "colormap": "Blues",
-        "ymax": {"im": 4.8e9, "stcl": 1.26e9, "nbody": 1.2e9},
+        "ymax": True,
     },
     "bus-cycles": {
         "label": "Bus Cycles",
         "colormap": "Blues",
-        "ymax": {"im": 1.1e8, "stcl": 5.8e7, "nbody": 3.6e7},
+        "ymax": True,
     },
     "cpu-cycles": {
         "label": "CPU Cycles",
         "colormap": "Blues",
-        "ymax": {"im": 2.3e10, "stcl": 1.3e10, "nbody": 7.4e9},
+        "ymax": True,
     },
     "cycle_activity.stalls_l1d_miss": {
         "label": "Cycles Stalled on L1D Miss",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 2.7e9, "stcl": 5.8e9, "nbody": 1.3e9},
+        "ymax": True,
     },
     "major-faults": {
         "label": "Major Faults",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 0.1, "stcl": 0.1, "nbody": 0.1},
+        "ymax": True,
     },
     "minor-faults": {
         "label": "Minor Faults",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 2.1e6, "stcl": 8.9e5, "nbody": 1250},
+        "ymax": True,
     },
     "frontend_retired.latency_ge_1": {
         "label": "Retired Instructions with Frontend Latency >= 1",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 7.3e8, "stcl": 3.5e8, "nbody": 2.6e6},
+        "ymax": True,
     },
     "resource_stalls.any": {
         "label": "Resource Stalls",
         "colormap": "RdYlGn_r",
-        "ymax": {"im": 9e9, "stcl": 7.5e9, "nbody": 3.8e9},
+        "ymax": True,
     },
 }
 
@@ -144,54 +145,65 @@ derived_values = {
 
 
 def plot_nmembers_heatmaps(df, app, scaled=False):
-    for layout in ["aos", "soa"]:
+    n_values = len(df.columns) - 3
+    rows = max(1, int(np.floor(np.log2(n_values)) + 1))
+    cols = max(len(derived_values), int(np.ceil(np.log2(n_values))))
+    ymax = np.zeros(rows * cols)
+
+    ymax = np.zeros((2, n_values + len(derived_values)))
+    imgs = []
+    figs = []
+    for il, layout in enumerate(["aos", "soa"]):
         df_layout = df[df["version"] == f"{layout}_manual"].copy()
-        plt.figure(figsize=(30, 17))
+        figs.append([layout, plt.figure(figsize=(30, 17))])
 
-        n_values = len(df.columns) - 3
-        rows = max(1, int(np.floor(np.log2(n_values)) + 1))
-        cols = max(len(derived_values), int(np.ceil(np.log2(n_values))))
-
+        ims = []
         for i, k in enumerate(df.columns[3:]):
             v = values[k]
 
             plt.subplot(rows, cols, i + 1)
             pivot = df_layout.pivot(index="before", columns="after", values=k)
-            plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower")
+            ims.append(plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower"))
             plt.colorbar(label=v["label"])
             plt.xlabel(r"Number of double data members $\bf{after}$")
             plt.ylabel(r"Number of double data members $\bf{before}$")
             plt.title(f"{v['label']}")
             plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
             plt.yticks(np.arange(len(pivot.index)), pivot.index)
-            if scaled and "ymax" in v and app in v["ymax"]:
-                plt.clim([0, v["ymax"][app]])
-            else:
-                plt.clim(vmin=0)
+            if v["ymax"]:
+                ymax[il][i] = np.max(pivot.values)
             plt.tight_layout()
 
         for i, (k, v) in enumerate(zip(derived_values.keys(), derived_values.values())):
             plt.subplot(rows, cols, i + n_values + 1)
             df_layout[k] = v["formula"](df_layout)
             pivot = df_layout.pivot(index="before", columns="after", values=k)
-            plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower")
+            ims.append(plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower"))
             plt.colorbar(label=v["label"])
             plt.xlabel(r"Number of double data members $\bf{after}$")
             plt.ylabel(r"Number of double data members $\bf{before}$")
             plt.title(f"{v['label']}")
             plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
             plt.yticks(np.arange(len(pivot.index)), pivot.index)
-            if scaled:
-                plt.clim(0, v["ymax"])
-            else:
-                plt.clim(vmin=0)
+            if v["ymax"]:
+                ymax[il][i + n_values] = np.max(pivot.values)
             plt.tight_layout()
 
-        plt.savefig(
+        imgs.append(ims)
+
+    if scaled:
+        for ic, ims_l in enumerate(imgs):
+            for ia, im in enumerate(ims_l):
+                max_val = np.max(ymax[:, ia])
+                im.set_clim(0, max_val) if max_val > 0 else im.set_clim(vmin=0)
+
+
+    for layout, fig in figs:
+        fig.savefig(
             f"images/heatmap_{layout}_nmembers_{app}{'_scaled' if scaled else ''}.png",
             bbox_inches="tight",
         )
-        plt.close()
+        plt.close(fig)
 
 
 def plot_stride_lines(app):
@@ -245,7 +257,7 @@ def plot_stride_lines(app):
 
 if __name__ == "__main__":
     for app in ["stcl", "im", "nbody"]:
-        for scaled in [False, True]:
+        for scaled in [True]:
             suffix = f"nmembers_{app}"
             if not os.path.exists(f"perf_output_{suffix}.csv"):
                 print(f"File perf_output_{suffix}.csv does not exist. Skipping...")
