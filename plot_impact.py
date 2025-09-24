a857ba43c682ee5c174079c89715c89394414e94
Jolly Chen <jolly.chen@student.uva.nl>
2025-09-24 14:51:48 +0200

    Fix ymax for derived values, more events
    Also fix output/input dir and output format globally

--- plot_impact.py
+++ plot_impact.py
@@ -3,61 +3,72 @@ import numpy as np
 import os
 import pandas as pd
 
+input_dir = "images/200925"
+output_dir = "images/200925"
+fmt = "png"
+
 values = {
-    "runtime_mean": {
-        "label": "Mean Runtime",
-        "colormap": "RdYlGn_r",
-        "ymax": True
-    },
+    "runtime_mean": {"label": "Mean Runtime", "colormap": "RdYlGn_r", "ymax": True},
     "runtime_stddev": {
         "label": "Standard Deviation of Runtime over 5 runs",
         "colormap": "RdYlGn_r",
-        "ymax": False
+        "ymax": False,
     },
     "instructions": {
         "label": "Number of Retired Instructions",
         "colormap": "Blues",
-        "ymax": True
+        "ymax": True,
     },
     "fp_arith_inst_retired.scalar": {
         "label": "Number of Scalar Instructions",
         "colormap": "RdYlGn_r",
-        "ymax": True
+        "ymax": True,
+    },
+    "fp_ops_retired_by_type.scalar_all": {
+        "label": "Number of Scalar Instructions",
+        "colormap": "RdYlGn_r",
+        "ymax": True,
     },
     "fp_arith_inst_retired.vector": {
         "label": "Number of Vector Instructions",
         "colormap": "RdYlGn",
-        "ymax": True
+        "ymax": True,
+    },
+    "fp_ops_retired_by_type.vector_all": {
+        "label": "Number of Vector Instructions",
+        "colormap": "RdYlGn",
+        "ymax": True,
     },
     "L1-icache-load-misses": {
         "label": "L1 Instruction Cache Load Misses",
         "colormap": "RdYlGn_r",
-        "ymax": True
+        "ymax": True,
     },
     "L1-dcache-loads": {
         "label": "L1 Data Cache Loads",
         "colormap": "Blues",
-        "ymax": True
+        "ymax": True,
     },
     "L1-dcache-load-misses": {
         "label": "L1 Data Cache Load Misses",
         "colormap": "RdYlGn_r",
-        "ymax": True
+        "ymax": True,
     },
     "cache-references": {
         "label": "L2 Cache References",
         "colormap": "Blues",
-        "ymax": True
+        "ymax": True,
     },
-    "cache-misses": {
-        "label": "L2 Cache Misses",
-        "colormap": "RdYlGn_r",
-        "ymax": True
-    },
+    "cache-misses": {"label": "L2 Cache Misses", "colormap": "RdYlGn_r", "ymax": True},
     "mem_inst_retired.all_loads": {
         "label": "Memory Loads Retired",
         "colormap": "Blues",
-        "ymax": True
+        "ymax": True,
+    },
+    "ls_dispatch.ld_dispatch": {
+        "label": "Memory Loads Retired",
+        "colormap": "Blues",
+        "ymax": True,
     },
     "alignment-faults": {
         "label": "Alignment Faults",
@@ -104,12 +115,51 @@ values = {
         "colormap": "RdYlGn_r",
         "ymax": True,
     },
+    "stalled-cycles-frontend": {
+        "label": "Stalled Cycles Frontend",
+        "colormap": "RdYlGn_r",
+        "ymax": True,
+    },
     "resource_stalls.any": {
         "label": "Resource Stalls",
         "colormap": "RdYlGn_r",
         "ymax": True,
     },
+    "de_no_dispatch_per_slot.backend_stalls": {
+        "label": "Backend Stalls",
+        "colormap": "RdYlGn_r",
+        "ymax": True,
+    },
 }
+
+
+def vector_utilization(df):
+    if "fp_arith_inst_retired.vector" in df and "fp_arith_inst_retired.scalar" in df:
+        return df["fp_arith_inst_retired.vector"] / (
+            df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]
+        )
+    elif (
+        "fp_ops_retired_by_type.vector_all" in df
+        and "fp_ops_retired_by_type.scalar_all" in df
+    ):
+        return df["fp_ops_retired_by_type.vector_all"] / (
+            df["fp_ops_retired_by_type.vector_all"]
+            + df["fp_ops_retired_by_type.scalar_all"]
+        )
+    else:
+        return np.nan
+
+
+def instructions_per_cycle(df):
+    if "instructions" in df:
+        return df["instructions"] / df["cpu-cycles"]
+    elif "fp_arith_inst_retired.vector" in df and "fp_arith_inst_retired.scalar" in df:
+        return (
+            df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]
+        ) / df["cpu-cycles"]
+    else:
+        return np.nan
+
 
 derived_values = {
     "l1-cache-ratio": {
@@ -121,8 +171,7 @@ derived_values = {
     "vector-utilization": {
         "label": "Vector Utilization",
         "colormap": "RdYlGn",
-        "formula": lambda df: df["fp_arith_inst_retired.vector"]
-        / (df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]),
+        "formula": lambda df: vector_utilization(df),
         "ymax": 1,
     },
     "branch-miss-ratio": {
@@ -134,11 +183,7 @@ derived_values = {
     "instructions-per-cycle": {
         "label": "Instructions Per Cycle",
         "colormap": "RdYlGn",
-        "formula": lambda df: df.get(
-            "instructions",
-            (df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]), # fallback if instructions not available
-        )
-        / df["cpu-cycles"],
+        "formula": lambda df: instructions_per_cycle(df),
         "ymax": 4,
     },
 }
@@ -146,9 +191,11 @@ derived_values = {
 
 def plot_nmembers_heatmaps(df, app, scaled=False):
     n_values = len(df.columns) - 3
-    rows = max(1, int(np.floor(np.log2(n_values)) + 1))
-    cols = max(len(derived_values), int(np.ceil(np.log2(n_values))))
-    ymax = np.zeros(rows * cols)
+    rows = max(1, int(np.ceil(np.log2(n_values)) + 1))
+    cols = max(len(derived_values), int(np.floor(np.log2(n_values))))
+    print(
+        f"Plotting {n_values + len(derived_values)} values in {rows} rows and {cols} columns"
+    )
 
     ymax = np.zeros((2, n_values + len(derived_values)))
     imgs = []
@@ -163,7 +210,9 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
 
             plt.subplot(rows, cols, i + 1)
             pivot = df_layout.pivot(index="before", columns="after", values=k)
-            ims.append(plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower"))
+            ims.append(
+                plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower")
+            )
             plt.colorbar(label=v["label"])
             plt.xlabel(r"Number of double data members $\bf{after}$")
             plt.ylabel(r"Number of double data members $\bf{before}$")
@@ -178,29 +227,29 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
             plt.subplot(rows, cols, i + n_values + 1)
             df_layout[k] = v["formula"](df_layout)
             pivot = df_layout.pivot(index="before", columns="after", values=k)
-            ims.append(plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower"))
+            ims.append(
+                plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower")
+            )
             plt.colorbar(label=v["label"])
             plt.xlabel(r"Number of double data members $\bf{after}$")
             plt.ylabel(r"Number of double data members $\bf{before}$")
             plt.title(f"{v['label']}")
             plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
             plt.yticks(np.arange(len(pivot.index)), pivot.index)
-            if v["ymax"]:
-                ymax[il][i + n_values] = np.max(pivot.values)
+            if "ymax" in v:
+                ymax[il][i + n_values] = v["ymax"]
             plt.tight_layout()
 
         imgs.append(ims)
 
-    if scaled:
-        for ic, ims_l in enumerate(imgs):
-            for ia, im in enumerate(ims_l):
-                max_val = np.max(ymax[:, ia])
-                im.set_clim(0, max_val) if max_val > 0 else im.set_clim(vmin=0)
-
+    for ims_l in imgs:
+        for ia, im in enumerate(ims_l):
+            max_val = np.max(ymax[:, ia])
+            im.set_clim(0, max_val) if scaled and max_val > 0 else im.set_clim(vmin=0)
 
     for layout, fig in figs:
         fig.savefig(
-            f"images/heatmap_{layout}_nmembers_{app}{'_scaled' if scaled else ''}.png",
+            f"{output_dir}/heatmap_{layout}_nmembers_{app}{'_scaled' if scaled else ''}.{fmt}",
             bbox_inches="tight",
         )
         plt.close(fig)
@@ -211,11 +260,11 @@ def plot_stride_lines(app):
     plt.suptitle(f"Invariant Mass with different Loop Strides", y=1.02, fontsize=16)
 
     suffix = f"{app}"
-    if not os.path.exists(f"perf_output_{suffix}.csv"):
-        print(f"File perf_output_{suffix}.csv does not exist. Skipping...")
+    if not os.path.exists(f"{input_dir}/perf_output_{suffix}.csv"):
+        print(f"File {input_dir}/perf_output_{suffix}.csv does not exist. Skipping...")
         return
 
-    with open(f"perf_output_{suffix}.csv", "r") as f:
+    with open(f"{input_dir}/perf_output_{suffix}.csv", "r") as f:
         df = pd.read_csv(f)
 
     for layout in ["aos", "soa"]:
@@ -251,19 +300,22 @@ def plot_stride_lines(app):
             plt.ylim(bottom=0, top=v["ymax"])
 
     plt.tight_layout()
-    plt.savefig(f"images/lines_{app}.png", bbox_inches="tight")
+    plt.savefig(f"{output_dir}/lines_{app}.{fmt}", bbox_inches="tight")
     plt.close()
 
 
 if __name__ == "__main__":
     for app in ["stcl", "im", "nbody"]:
-        for scaled in [True]:
+        for scaled in [False, True]:
             suffix = f"nmembers_{app}"
-            if not os.path.exists(f"perf_output_{suffix}.csv"):
-                print(f"File perf_output_{suffix}.csv does not exist. Skipping...")
+            if not os.path.exists(f"{input_dir}/perf_output_{suffix}.csv"):
+                print(
+                    f"File {input_dir}/perf_output_{suffix}.csv does not exist. Skipping..."
+                )
                 continue
 
-            with open(f"perf_output_{suffix}.csv", "r") as f:
+            print(f"Reading {input_dir}/perf_output_{suffix}.csv")
+            with open(f"{input_dir}/perf_output_{suffix}.csv", "r") as f:
                 df = pd.read_csv(f)
 
             plot_nmembers_heatmaps(df, app, scaled)
