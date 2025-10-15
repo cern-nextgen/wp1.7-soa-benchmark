ed42f361488778e5ff09fc2e72bb90636829e706
Jolly Chen <jolly.chen@student.uva.nl>
2025-10-15 10:30:06 +0200

    Plot arithmetic intensity
    Add more performance events
    
    Set input/output dir via global var

--- plot_impact.py
+++ plot_impact.py
@@ -2,13 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import glob
 
-input_dir = "images/200925"
-output_dir = "images/200925"
-fmt = "png"
+
 
 values = {
-    "runtime_mean": {"label": "Mean Runtime", "colormap": "RdYlGn_r", "ymax": True},
+    "runtime_mean": {"label": "Mean Runtime (ms)", "colormap": "RdYlGn_r", "ymax": True},
     "runtime_stddev": {
         "label": "Standard Deviation of Runtime over 5 runs",
         "colormap": "RdYlGn_r",
@@ -20,7 +19,7 @@ values = {
         "ymax": True,
     },
     "fp_arith_inst_retired.scalar": {
-        "label": "Number of Scalar Instructions",
+    "label": "Number of Scalar Instructions",
         "colormap": "RdYlGn_r",
         "ymax": True,
     },
@@ -65,8 +64,18 @@ values = {
         "colormap": "Blues",
         "ymax": True,
     },
+    "mem_inst_retired.any": {
+        "label": "Retired Memory Instructions",
+        "colormap": "Blues",
+        "ymax": True,
+    },
     "ls_dispatch.ld_dispatch": {
-        "label": "Memory Loads Retired",
+        "label": "Dispatched Load Operations",
+        "colormap": "Blues",
+        "ymax": True,
+    },
+    "ls_dispatch.ld_st_dispatch": {
+        "label": "Dispatched Load-Store Operations",
         "colormap": "Blues",
         "ymax": True,
     },
@@ -97,6 +106,16 @@ values = {
     },
     "cycle_activity.stalls_l1d_miss": {
         "label": "Cycles Stalled on L1D Miss",
+        "colormap": "RdYlGn_r",
+        "ymax": True,
+    },
+    "cycle_activity.stalls_mem_any": {
+        "label": "Cycles Stalled on Memory Access",
+        "colormap": "RdYlGn_r",
+        "ymax": True,
+    },
+    "ex_no_retire.load_not_complete": {
+        "label": "Cycles Stalled on Load Not Complete",
         "colormap": "RdYlGn_r",
         "ymax": True,
     },
@@ -147,7 +166,7 @@ def vector_utilization(df):
             + df["fp_ops_retired_by_type.scalar_all"]
         )
     else:
-        return np.nan
+        raise ValueError("No suitable columns for vector utilization")
 
 
 def instructions_per_cycle(df):
@@ -158,8 +177,36 @@ def instructions_per_cycle(df):
             df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]
         ) / df["cpu-cycles"]
     else:
-        return np.nan
+        raise ValueError("No suitable columns for instructions per cycle")
 
+def arithmetic_intensity(df):
+    if "fp_arith_inst_retired.vector" in df and "fp_arith_inst_retired.scalar" in df:
+        arith = df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]
+    elif "fp_ops_retired_by_type.vector_all" in df and "fp_ops_retired_by_type.scalar_all" in df:
+        arith = df["fp_ops_retired_by_type.vector_all"] + df["fp_ops_retired_by_type.scalar_all"]
+    else:
+        raise ValueError("No suitable columns for arithmetic intensity", df.columns)
+
+    if "mem_inst_retired.all_loads" in df:
+        mem = df["mem_inst_retired.all_loads"]
+    elif "mem_inst_retired.any" in df:
+        mem = df["mem_inst_retired.any"]
+    elif "ls_dispatch.ld_dispatch" in df:
+        mem = df["ls_dispatch.ld_dispatch"]
+    elif "ls_dispatch.ld_st_dispatch" in df:
+        mem = df["ls_dispatch.ld_st_dispatch"]
+    else:
+        raise ValueError("No suitable columns for memory operations", df.columns)
+
+    return arith / mem
+
+def arithmetic_intensity_label(df):
+    if "mem_inst_retired.all_loads" in df or "ls_dispatch.ld_dispatch" in df:
+        return "Arithmetic Intensity (FLOP/Mem Loads)"
+    elif "mem_inst_retired.any" in df or "ls_dispatch.ld_st_dispatch" in df:
+        return "Arithmetic Intensity (FLOP/Mem Instructions)"
+    else:
+        raise ValueError("No suitable columns for AI label", df.columns)
 
 derived_values = {
     "l1-cache-ratio": {
@@ -168,10 +215,16 @@ derived_values = {
         "formula": lambda df: df["L1-dcache-load-misses"] / df["L1-dcache-loads"],
         "ymax": 1,
     },
-    "vector-utilization": {
-        "label": "Vector Utilization",
+    # "vector-utilization": {
+    #     "label": "Vector Utilization",
+    #     "colormap": "RdYlGn",
+    #     "formula": lambda df: vector_utilization(df),
+    #     "ymax": 1,
+    # },
+    "arithmetic-intensity": {
+        "label": lambda df: arithmetic_intensity_label(df),
         "colormap": "RdYlGn",
-        "formula": lambda df: vector_utilization(df),
+        "formula": lambda df: arithmetic_intensity(df),
         "ymax": 1,
     },
     "branch-miss-ratio": {
@@ -224,7 +277,7 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
             plt.tight_layout()
 
         for i, (k, v) in enumerate(zip(derived_values.keys(), derived_values.values())):
-            plt.subplot(rows, cols, i + n_values + 1)
+            plt.subplot(rows, cols, i + 17)
             df_layout[k] = v["formula"](df_layout)
             pivot = df_layout.pivot(index="before", columns="after", values=k)
             ims.append(
@@ -233,7 +286,7 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
             plt.colorbar(label=v["label"])
             plt.xlabel(r"Number of double data members $\bf{after}$")
             plt.ylabel(r"Number of double data members $\bf{before}$")
-            plt.title(f"{v['label']}")
+            plt.title(f"{v['label']}" if not v == "arithmetic-intensity" else v['label'](df_layout))
             plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
             plt.yticks(np.arange(len(pivot.index)), pivot.index)
             if "ymax" in v:
@@ -248,10 +301,11 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
             im.set_clim(0, max_val) if scaled and max_val > 0 else im.set_clim(vmin=0)
 
     for layout, fig in figs:
-        fig.savefig(
-            f"{output_dir}/heatmap_{layout}_nmembers_{app}{'_scaled' if scaled else ''}.{fmt}",
-            bbox_inches="tight",
-        )
+        for fmt in formats:
+            fig.savefig(
+                f"{output_dir}/heatmap_{layout}_nmembers_{app}{'_scaled' if scaled else ''}.{fmt}",
+                bbox_inches="tight",
+            )
         plt.close(fig)
 
 
@@ -271,8 +325,8 @@ def plot_stride_lines(app):
         df_layout = df[df["version"] == f"{layout}_manual"].copy()
 
         n_values = len(df.columns) - 2
-        rows = max(1, int(np.floor(np.log2(n_values)) + 1))
-        cols = max(len(derived_values), int(np.ceil(np.log2(n_values))))
+        rows = max(1, int(np.ceil(np.log2(n_values)) + 1))
+        cols = max(len(derived_values), int(np.floor(np.log2(n_values))))
 
         for i, k in enumerate(df.columns[2:]):
             v = values[k]
@@ -281,6 +335,10 @@ def plot_stride_lines(app):
             plt.plot(
                 df_layout["stride"], df_layout[k], marker="o", label=layout.upper()
             )
+
+            if v['label'] == 'Mean Runtime (ms)':
+                plt.yscale('symlog')
+
             plt.xlabel("Loop stride")
             plt.ylabel(v["label"])
             plt.legend()
@@ -290,7 +348,7 @@ def plot_stride_lines(app):
         # derived values
         for i, (k, v) in enumerate(zip(derived_values.keys(), derived_values.values())):
             df_layout[k] = v["formula"](df_layout)
-            plt.subplot(rows, cols, i + n_values + 1)
+            plt.subplot(rows, cols, i + 17)
             plt.plot(
                 df_layout["stride"], df_layout[k], marker="o", label=layout.upper()
             )
@@ -300,25 +358,26 @@ def plot_stride_lines(app):
             plt.ylim(bottom=0, top=v["ymax"])
 
     plt.tight_layout()
-    plt.savefig(f"{output_dir}/lines_{app}.{fmt}", bbox_inches="tight")
+    for fmt in formats:
+        plt.savefig(f"{output_dir}/lines_{app}.{fmt}", bbox_inches="tight")
     plt.close()
 
 
+# input_dir  = "/home/jollychen/soa-benchmark/wp1.7-soa-benchmark"
+input_dir  = "/data/soa-benchmark-results/251001/ngt"
+output_dir = "/data/soa-benchmark-results/251001/ngt"
+formats = ["png", "pdf"]
+
 if __name__ == "__main__":
-    for app in ["stcl", "im", "nbody"]:
-        for scaled in [False, True]:
-            suffix = f"nmembers_{app}"
-            if not os.path.exists(f"{input_dir}/perf_output_{suffix}.csv"):
-                print(
-                    f"File {input_dir}/perf_output_{suffix}.csv does not exist. Skipping..."
-                )
-                continue
+    nmembers_files = glob.glob(os.path.join(input_dir, "*nmembers*.csv"))
+    for nm_file in nmembers_files:
+        print(f"Processing file: {nm_file}")
+        app = os.path.splitext(os.path.basename(nm_file))[0].split("perf_output_")[1]
+        plot_nmembers_heatmaps(pd.read_csv(nm_file), app, scaled=False)
+        plot_nmembers_heatmaps(pd.read_csv(nm_file), app, scaled=True)
 
-            print(f"Reading {input_dir}/perf_output_{suffix}.csv")
-            with open(f"{input_dir}/perf_output_{suffix}.csv", "r") as f:
-                df = pd.read_csv(f)
-
-            plot_nmembers_heatmaps(df, app, scaled)
-
-    app = "stride_im"
-    plot_stride_lines(app)
+    stride_files = glob.glob(os.path.join(input_dir, "*stride*im*.csv"))
+    for s_file in stride_files:
+        print(f"Processing file: {s_file}")
+        app = os.path.splitext(os.path.basename(s_file))[0].split("perf_output_")[1]
+        plot_stride_lines(app)
