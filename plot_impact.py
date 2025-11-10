59b766c6559420d9bbfe9b12d5540fdc30cd0eaf
Jolly Chen <jolly.chen@student.uva.nl>
2025-11-10 17:21:38 +0100

    Add plotting func for nmembers+stride

--- plot_impact.py
+++ plot_impact.py
@@ -5,9 +5,12 @@ import pandas as pd
 import glob
 
 
-
 values = {
-    "runtime_mean": {"label": "Mean Runtime (ms)", "colormap": "RdYlGn_r", "ymax": True},
+    "runtime_mean": {
+        "label": "Mean Runtime (ms)",
+        "colormap": "RdYlGn_r",
+        "ymax": True,
+    },
     "runtime_stddev": {
         "label": "Standard Deviation of Runtime over 5 runs",
         "colormap": "RdYlGn_r",
@@ -19,7 +22,7 @@ values = {
         "ymax": True,
     },
     "fp_arith_inst_retired.scalar": {
-    "label": "Number of Scalar Instructions",
+        "label": "Number of Scalar Instructions",
         "colormap": "RdYlGn_r",
         "ymax": True,
     },
@@ -179,11 +182,18 @@ def instructions_per_cycle(df):
     else:
         raise ValueError("No suitable columns for instructions per cycle")
 
+
 def arithmetic_intensity(df):
     if "fp_arith_inst_retired.vector" in df and "fp_arith_inst_retired.scalar" in df:
         arith = df["fp_arith_inst_retired.vector"] + df["fp_arith_inst_retired.scalar"]
-    elif "fp_ops_retired_by_type.vector_all" in df and "fp_ops_retired_by_type.scalar_all" in df:
-        arith = df["fp_ops_retired_by_type.vector_all"] + df["fp_ops_retired_by_type.scalar_all"]
+    elif (
+        "fp_ops_retired_by_type.vector_all" in df
+        and "fp_ops_retired_by_type.scalar_all" in df
+    ):
+        arith = (
+            df["fp_ops_retired_by_type.vector_all"]
+            + df["fp_ops_retired_by_type.scalar_all"]
+        )
     else:
         raise ValueError("No suitable columns for arithmetic intensity", df.columns)
 
@@ -200,6 +210,7 @@ def arithmetic_intensity(df):
 
     return arith / mem
 
+
 def arithmetic_intensity_label(df):
     if "mem_inst_retired.all_loads" in df or "ls_dispatch.ld_dispatch" in df:
         return "Arithmetic Intensity (FLOP/Mem Loads)"
@@ -207,6 +218,7 @@ def arithmetic_intensity_label(df):
         return "Arithmetic Intensity (FLOP/Mem Instructions)"
     else:
         raise ValueError("No suitable columns for AI label", df.columns)
+
 
 derived_values = {
     "l1-cache-ratio": {
@@ -225,7 +237,7 @@ derived_values = {
         "label": lambda df: arithmetic_intensity_label(df),
         "colormap": "RdYlGn",
         "formula": lambda df: arithmetic_intensity(df),
-        "ymax": 1,
+        # "ymax": 1,
     },
     "branch-miss-ratio": {
         "label": "Branch Miss Ratio",
@@ -241,8 +253,7 @@ derived_values = {
     },
 }
 
-
-def plot_nmembers_heatmaps(df, app, scaled=False):
+def plot_nmembers_heatmaps(df, app, scaled, output_dir):
     n_values = len(df.columns) - 3
     rows = max(1, int(np.ceil(np.log2(n_values)) + 1))
     cols = max(len(derived_values), int(np.floor(np.log2(n_values))))
@@ -286,7 +297,11 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
             plt.colorbar(label=v["label"])
             plt.xlabel(r"Number of double data members $\bf{after}$")
             plt.ylabel(r"Number of double data members $\bf{before}$")
-            plt.title(f"{v['label']}" if not v == "arithmetic-intensity" else v['label'](df_layout))
+            plt.title(
+                f"{v['label']}"
+                if not k == "arithmetic-intensity"
+                else v["label"](df_layout)
+            )
             plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
             plt.yticks(np.arange(len(pivot.index)), pivot.index)
             if "ymax" in v:
@@ -303,23 +318,15 @@ def plot_nmembers_heatmaps(df, app, scaled=False):
     for layout, fig in figs:
         for fmt in formats:
             fig.savefig(
-                f"{output_dir}/heatmap_{layout}_nmembers_{app}{'_scaled' if scaled else ''}.{fmt}",
+                f"{output_dir}/heatmap_{layout}_{app}{'_scaled' if scaled else ''}.{fmt}",
                 bbox_inches="tight",
             )
         plt.close(fig)
 
 
-def plot_stride_lines(app):
+def plot_stride_lines(df, app, output_dir):
     plt.figure(figsize=(25, 15))
     plt.suptitle(f"Invariant Mass with different Loop Strides", y=1.02, fontsize=16)
-
-    suffix = f"{app}"
-    if not os.path.exists(f"{input_dir}/perf_output_{suffix}.csv"):
-        print(f"File {input_dir}/perf_output_{suffix}.csv does not exist. Skipping...")
-        return
-
-    with open(f"{input_dir}/perf_output_{suffix}.csv", "r") as f:
-        df = pd.read_csv(f)
 
     for layout in ["aos", "soa"]:
         df_layout = df[df["version"] == f"{layout}_manual"].copy()
@@ -336,8 +343,8 @@ def plot_stride_lines(app):
                 df_layout["stride"], df_layout[k], marker="o", label=layout.upper()
             )
 
-            if v['label'] == 'Mean Runtime (ms)':
-                plt.yscale('symlog')
+            if v["label"] == "Mean Runtime (ms)":
+                plt.yscale("symlog")
 
             plt.xlabel("Loop stride")
             plt.ylabel(v["label"])
@@ -353,31 +360,173 @@ def plot_stride_lines(app):
                 df_layout["stride"], df_layout[k], marker="o", label=layout.upper()
             )
             plt.xlabel("Loop stride")
-            plt.ylabel(v["label"])
+            plt.ylabel(
+                f"{v['label']}"
+                if not k == "arithmetic-intensity"
+                else v["label"](df_layout)
+            )
             plt.legend()
-            plt.ylim(bottom=0, top=v["ymax"])
+            plt.ylim(bottom=0, top=v.get("ymax", None))
 
     plt.tight_layout()
     for fmt in formats:
         plt.savefig(f"{output_dir}/lines_{app}.{fmt}", bbox_inches="tight")
     plt.close()
 
+def plot_nmembers_stride(df, app, scaled, output_dir):
+    n_values = len(df.columns) - 4
+    rows = max(1, int(np.ceil(np.log2(n_values)) + 1))
+    cols = max(len(derived_values), int(np.floor(np.log2(n_values))))
+    print(
+        f"Plotting {n_values + len(derived_values)} values in {rows} rows and {cols} columns"
+    )
 
-# input_dir  = "/home/jollychen/soa-benchmark/wp1.7-soa-benchmark"
-input_dir  = "/data/soa-benchmark-results/251001/ngt"
-output_dir = "/data/soa-benchmark-results/251001/ngt"
+    ymax = np.zeros((2, n_values + len(derived_values)))
+    imgs = []
+    figs = []
+    for il, layout in enumerate(["aos", "soa"]):
+        df_layout = df[df["version"] == f"{layout}_manual"].copy()
+        df_layout = df_layout[df_layout["after"] == 0]
+        figs.append([layout, plt.figure(figsize=(30, 17))])
+
+        ims = []
+        for i, k in enumerate(df.columns[4:]):
+            v = values[k]
+
+            plt.subplot(rows, cols, i + 1)
+            pivot = df_layout.pivot(index="before", columns="stride", values=k)
+            ims.append(
+                plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower")
+            )
+            plt.colorbar(label=v["label"])
+            plt.xlabel(r"Number of double padding data members")
+            plt.ylabel(r"Loop stride")
+            plt.title(f"{v['label']}")
+            plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
+            plt.yticks(np.arange(len(pivot.index)), pivot.index)
+            if v["ymax"]:
+                ymax[il][i] = np.max(pivot.values)
+            plt.tight_layout()
+
+        for i, (k, v) in enumerate(zip(derived_values.keys(), derived_values.values())):
+            plt.subplot(rows, cols, i + 17)
+            df_layout[k] = v["formula"](df_layout)
+            pivot = df_layout.pivot(index="before", columns="stride", values=k)
+            ims.append(
+                plt.imshow(pivot, cmap=v["colormap"], aspect="auto", origin="lower")
+            )
+            plt.colorbar(label=v["label"])
+            plt.xlabel(r"Number of double padding data members")
+            plt.ylabel(r"Loop stride")
+            plt.title(
+                f"{v['label']}"
+                if not k == "arithmetic-intensity"
+                else v["label"](df_layout)
+            )
+            plt.xticks(np.arange(len(pivot.columns)), pivot.columns)
+            plt.yticks(np.arange(len(pivot.index)), pivot.index)
+            if "ymax" in v:
+                ymax[il][i + n_values] = v["ymax"]
+            plt.tight_layout()
+
+        imgs.append(ims)
+
+    for ims_l in imgs:
+        for ia, im in enumerate(ims_l):
+            max_val = np.max(ymax[:, ia])
+            im.set_clim(0, max_val) if scaled and max_val > 0 else im.set_clim(vmin=0)
+
+    for layout, fig in figs:
+        for fmt in formats:
+            fig.savefig(
+                f"{output_dir}/heatmap_{layout}_{app}{'_scaled' if scaled else ''}.{fmt}",
+                bbox_inches="tight",
+            )
+        plt.close(fig)
+
+def plot_stride_x(df, app, output_dir, x):
+    plt.figure(figsize=(6, 4))
+
+    for layout in ["aos", "soa"]:
+        df_layout = df[df["version"] == f"{layout}_manual"].copy()
+
+        plt.plot(
+            df_layout["stride"],
+            df_layout[x],
+            marker="o",
+            label=layout.upper(),
+        )
+
+    # plt.yscale("symlog")
+    plt.xlabel("Loop stride")
+    # plt.ylabel("Mean Runtime (ms)")
+    plt.ylabel(values[x]["label"])
+    plt.title(f"Invariant Mass Number of Arithmetic Vector Instructions vs Loop Stride")
+    plt.legend()
+    plt.tight_layout()
+    for fmt in formats:
+        plt.savefig(f"{output_dir}/vector_instr_lines_{app}.{fmt}", bbox_inches="tight")
+    plt.close()
+
 formats = ["png", "pdf"]
 
 if __name__ == "__main__":
-    nmembers_files = glob.glob(os.path.join(input_dir, "*nmembers*.csv"))
-    for nm_file in nmembers_files:
-        print(f"Processing file: {nm_file}")
-        app = os.path.splitext(os.path.basename(nm_file))[0].split("perf_output_")[1]
-        plot_nmembers_heatmaps(pd.read_csv(nm_file), app, scaled=False)
-        plot_nmembers_heatmaps(pd.read_csv(nm_file), app, scaled=True)
+    inputs = [
+        # "251008/ngt",
+        # "251007/ngt",
+        # "251001/ngt",
+        # "250929/ngt",
+        # "250926/ngt",
+        # "251013/ngt",
 
-    stride_files = glob.glob(os.path.join(input_dir, "*stride*im*.csv"))
-    for s_file in stride_files:
-        print(f"Processing file: {s_file}")
-        app = os.path.splitext(os.path.basename(s_file))[0].split("perf_output_")[1]
-        plot_stride_lines(app)
+        "251007/local",
+        # "251001/local",
+        # "250919/local",
+        # "251013/local",
+    ]
+
+    for input_dir in [f"/data/soa-benchmark-results/{i}" for i in inputs]:
+        output_dir = input_dir
+
+        nmembers_files = [
+            f
+            for f in glob.glob(os.path.join(input_dir, "*nmembers*.csv"))
+            if "nmembers_stride" not in os.path.basename(f)
+        ]
+        for nm_file in nmembers_files:
+            print(f"Processing file: {nm_file}")
+            app = os.path.splitext(os.path.basename(nm_file))[0].split("perf_output_")[1]
+            for scaled in [False, True]:
+                plot_nmembers_heatmaps(
+                    pd.read_csv(nm_file),
+                    app,
+                    scaled=scaled,
+                    output_dir=output_dir,
+                )
+
+        stride_files = [
+            f
+            for f in glob.glob(os.path.join(input_dir, "*stride*im*.csv"))
+            if "nmembers_stride" not in os.path.basename(f)
+        ]
+        for s_file in stride_files:
+            print(f"Processing file: {s_file}")
+            app = os.path.splitext(os.path.basename(s_file))[0].split("perf_output_")[1]
+            plot_stride_lines(pd.read_csv(s_file), app, output_dir=output_dir)
+            plot_stride_x(pd.read_csv(s_file), app, output_dir=output_dir, x="fp_arith_inst_retired.vector")
+
+        nmembers_stride_files = [
+            f
+            for f in glob.glob(os.path.join(input_dir, "*nmembers_stride*.csv"))
+        ]
+        for nms_file in nmembers_stride_files:
+            print(f"Processing file: {nms_file}")
+            app = os.path.splitext(os.path.basename(nms_file))[0].split("perf_output_")[1]
+            for scaled in [False, True]:
+                plot_nmembers_stride(
+                    pd.read_csv(nms_file),
+                    app,
+                    scaled=scaled,
+                    output_dir=output_dir,
+                )
+                
