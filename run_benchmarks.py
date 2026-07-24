951bd78da2ba23b699d5549be4c413827c559358
leobeltra <leonardo.beltrame@polimi.it>
2026-07-24 03:02:00 +0200

    Compile and run with Clang

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 if __name__ == "__main__":
 
+    benchmark_filter = sys.argv[3] if len(sys.argv) > 3 else "BM_stencil"
+
     if len(sys.argv) > 2:
         output_dir = sys.argv[1]
         csvfile = sys.argv[2]
@@ -16,8 +18,8 @@ if __name__ == "__main__":
             print("Running the benchmarks...")
             for f in soa_versions:
                 filename = f"{output_dir}/{f}"
-                subprocess.run([f"{filename}", "--benchmark_out_format=json", f"--benchmark_out={filename}.json", "--benchmark_min_time=30x",
+                subprocess.run([f"{filename}", f"--benchmark_filter={benchmark_filter}", "--benchmark_out_format=json", f"--benchmark_out={filename}.json", "--benchmark_min_time=30x",
                                 "--benchmark_counters_tabular=true", "--benchmark_repetitions=3", "--benchmark_min_warmup_time=2"])
     else:
-        print("python run_benchmarks.py <output_dir> <csvfile>")
+        print("python3 run_benchmarks.py <output_dir> <csvfile> [benchmark_filter]")
         print("Provide a CSV file with the SoA versions to benchmark and their labels")
--- soa_boost.cpp
+++ soa_boost.cpp
@@ -166,7 +166,7 @@ template <typename SoA, typename N>
 class Fixture1 : public benchmark::Fixture {
  public:
     std::byte *buffer;
-    using SoAView = typename SoA::template ViewTemplate<cms::soa::RestrictQualify::enabled, cms::soa::RangeChecking::Default>;
+    using SoAView = typename SoA::template ViewTemplate<cms::soa::RestrictQualify::enabled, cms::soa::RangeChecking::disabled>;
     SoAView t;
 
     static constexpr auto n = N::value;
